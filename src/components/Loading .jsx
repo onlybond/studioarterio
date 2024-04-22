f8e9fe24741bdbfe2e375695c13290bb7b2f7136
onlybond <36098799+onlybond@users.noreply.github.com>
2024-04-22 11:54:48 +0530

    refactor

--- src/components/Loading .jsx
+++ src/components/Loading .jsx
@@ -1,30 +1,24 @@
 import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-
+import Logo from "../assets/Logo";
 const Loading = () => {
   const containerRef = useRef();
 
   useGSAP(() => {
     const tl = gsap.timeline();
-    const letters = Array.from(document.querySelectorAll('.logotext'));
-    const shuffledLetters = shuffleArray(letters); // Shuffle the array of letters
-    tl.staggerFromTo(shuffledLetters, 0.7, { opacity: 0 }, { opacity: 1, ease: "power2.inOut" },0.1); // Random stagger animation
-    tl.to(containerRef.current, {
-      y: "-100%",
-      duration:1.5,
-      ease: "power2.inOut",
-    });
+    tl.to(
+      containerRef.current,
+      1,
+      {
+        y:"-100%",
+        duration:1.5,
+        delay:1.5,
+        ease: "power2.inOut"
+      }
+    )
   });
 
-  // Function to shuffle the array
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
 
   return (
     <div
@@ -32,23 +26,7 @@ const Loading = () => {
       ref={containerRef}
       style={{ position: "absolute", bottom: 0 }}
     >
-      <div className="flex items-center gap-2 justify-center">
-        <span className="font-[NeueKabel] text-3xl text-white opacity-0 logotext">S</span>
-        <span className="font-[NeueKabel] text-3xl text-white opacity-0 logotext">T</span>
-        <span className="font-[NeueKabel] text-3xl text-white opacity-0 logotext">U</span>
-        <span className="font-[NeueKabel] text-3xl text-white opacity-0 logotext">D</span>
-        <span className="font-[NeueKabel] text-3xl text-white opacity-0 logotext">I</span>
-        <span className="font-[NeueKabel] text-3xl text-white opacity-0 logotext">O</span>
-      </div>
-      <div className="flex items-center gap-2 justify-center">
-        <span className="font-[NeueKabel] text-7xl text-white opacity-0 logotext">A</span>
-        <span className="font-[NeueKabel] text-7xl text-white opacity-0 logotext">R</span>
-        <span className="font-[NeueKabel] text-7xl text-white opacity-0 logotext">T</span>
-        <span className="font-[NeueKabel] text-7xl text-white opacity-0 logotext">E</span>
-        <span className="font-[NeueKabel] text-7xl text-white opacity-0 logotext">R</span>
-        <span className="font-[NeueKabel] text-7xl text-white opacity-0 logotext">I</span>
-        <span className="font-[NeueKabel] text-7xl text-white opacity-0 logotext">O</span>
-      </div>
+      <Logo/>
     </div>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import Hamburger from "./Hamburger";
 import logo from "../assets/logo.svg";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const Nav = () => {
+  useGSAP(()=>{
+    const tl = gsap.timeline();
+    const letters = Array.from(document.querySelectorAll('.logotext'));
+    const shuffledLetters = shuffleArray(letters); // Shuffle the array of letters
+    tl.staggerFromTo(shuffledLetters, 0.7, { opacity: 0 }, { opacity: 1,delay:2, ease: "power2.inOut" },0.1);
+  })
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
   return (
     <div className="flex justify-between items-center py-5 sm:py-8 px-5 sm:px-12 fixed top-0 left-0 w-full h-fit z-10 bg-white">
       <div className="flex items-center gap-2">
-      <img src={logo} alt="" className="w-14 h-14 sm:w-10 sm:h-10 absolute sm:left-0 sm:translate-x-0 left-1/2 -translate-x-1/2 sm:relative" />
-      <h1 className="font-[NeueKabel] font-black hidden sm:inline-block text-2xl">Studio Arterio</h1>
+      <div className="flex items-center gap-2 justify-center">
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">S</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">T</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">U</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">D</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">I</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">O</span>
+      </div>
+      <div className="flex items-center gap-2 justify-center">
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">A</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">R</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">T</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">E</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">R</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">I</span>
+        <span className="font-[NeueKabel] text-3xl opacity-0 logotext">O</span>
+      </div>
       </div>
       <Hamburger />
     </div>
